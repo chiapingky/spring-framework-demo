b3bb953ae78543a18d172c901c15bef0288f9e9b
chiapingky <pingky99@gmail.com>
2023-09-16 00:38:02 +0700

    Create Room Repository h2 Database

---
+++ hotel-service/src/main/java/com/chiapingky/hotel/repository/room/RoomRepository.java
@@ -0,0 +1,6 @@
+package com.chiapingky.hotel.repository.room;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoomRepository extends CrudRepository<Room, Long> {
+}
---
+++ hotel-service/src/main/java/com/chiapingky/hotel/utils/AppStartupEvent.java
@@ -0,0 +1,22 @@
+package com.chiapingky.hotel.utils;
+
+import com.chiapingky.hotel.repository.room.Room;
+import com.chiapingky.hotel.repository.room.RoomRepository;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppStartupEvent implements ApplicationListener<ApplicationReadyEvent> {
+    private final RoomRepository roomRepository;
+
+    public AppStartupEvent(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        Iterable<Room> rooms = roomRepository.findAll();
+        rooms.forEach(System.out::println);
+    }
+}
