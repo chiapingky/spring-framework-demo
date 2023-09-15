30ebb7c634b54f9936913fabe059a04c183ed56a
chiapingky <pingky99@gmail.com>
2023-09-16 02:06:42 +0700

    Add Find Reservation By Date Capability

--- hotel-service/src/main/java/com/chiapingky/hotel/repository/room/ReservationRepository.java
+++ hotel-service/src/main/java/com/chiapingky/hotel/repository/room/ReservationRepository.java
@@ -1,9 +1,9 @@
 package com.chiapingky.hotel.repository.room;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.sql.Date;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-    Iterable<Reservation> getReservationsByDate(Date date);
+    Iterable<Reservation> findReservationByReservationDate(Date date);
 }
--- hotel-service/src/main/java/com/chiapingky/hotel/utils/AppStartupEvent.java
+++ hotel-service/src/main/java/com/chiapingky/hotel/utils/AppStartupEvent.java
@@ -1,28 +1,32 @@
 package com.chiapingky.hotel.utils;
 
+import com.chiapingky.hotel.business.ReservationService;
 import com.chiapingky.hotel.repository.room.*;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
+import java.sql.Date;
 
 @Component
 public class AppStartupEvent implements ApplicationListener<ApplicationReadyEvent> {
     private final RoomRepository roomRepository;
     private final GuestRepository guestRepository;
     private final ReservationRepository reservationRepository;
+    private final ReservationService reservationService;
+    private final DateUtils dateUtils;
 
     public AppStartupEvent(
             RoomRepository roomRepository,
             GuestRepository guestRepository,
-            ReservationRepository reservationRepository
+            ReservationRepository reservationRepository,
+            ReservationService reservationService
     ) {
         this.roomRepository = roomRepository;
         this.guestRepository = guestRepository;
         this.reservationRepository = reservationRepository;
+        this.reservationService = reservationService;
+        dateUtils = new DateUtils();
     }
 
     @Override
@@ -33,5 +37,6 @@ public class AppStartupEvent implements ApplicationListener<ApplicationReadyEven
         guests.forEach(System.out::println);
         Iterable<Reservation> reservations = reservationRepository.findAll();
         reservations.forEach(System.out::println);
+        System.out.println(reservationService.getRoomReservationsForDate(new Date(2022, 0, 1)));
     }
 }
